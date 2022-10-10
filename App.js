b46b0913b56b1e0c6f0c73482388db522e5922c8
Diallo Abdoulaye <lingodir@gmail.com>
2022-10-09 23:45:40 -0400

    lists props

--- App.js
+++ App.js
@@ -11,8 +11,7 @@ import { SafeAreaView,StatusBar} from 'react-native';
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
 import feed from './assets/data/feed';
-
-
+import SearchResultsScreen from './src/screens/SearchResults';
 
 const post1 = feed[0];
 
@@ -23,7 +22,8 @@ const App: () => React$Node = () => {
     <StatusBar barStyle="dark-content" />
     <SafeAreaView >
         {/*<HomeScreen /> */}
-        <Post post={post1} />
+        { /*<Post post={post1} />*/}
+        <SearchResultsScreen />
 
     </SafeAreaView>
     </>
